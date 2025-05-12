d3c5155044fb261defd78b7342137a5f17003fe3
Luiz Felipe Mascarenhas Dalle Nery <lmascare@fgrenoble.grenoble.grid5000.fr>
2025-05-12 12:47:46 +0200

    adapting scripts

---
+++ scripts/launch_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source $PWD/scripts/launch_dask.sh
+
+source $PWD/scripts/launch_analytics.sh
---
+++ scripts/launch_analytics.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xeu
+
+OPWD=$PWD
+
+# set NDASKWORKERS, NMPI, SCHEFILE
+source $PWD/scripts/env.sh
+
+EXPERIMENT_DIR=$PWD/experiment
+cd $EXPERIMENT_DIR
+
+python3 $OPWD/in-situ/bench_deisa.py $NDASKWORKERS $SCHEFILE 2>analytics.e&
+
+PYTHON_PID=$!
+
+SIM_EXE=$OPWD/simulation/build/main
+S_INI=$OPWD/simulation/setup.ini
+PDI_DEISA_YML=$OPWD/simulation/io_deisa.yml
+
+# $SIM_EXE $S_INI $IO_DEISA
+pdirun mpirun -np $NMPI $SIM_EXE $S_INI $IO_DEISA --kokkos-map-device-id-by=mpi_rank 
+
+wait $PYTHON_PID
+cd $OPWD
+set +xeu
