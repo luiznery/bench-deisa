90346869251e6f3ee84596e0638a09c4407cc611
Luiz Felipe Mascarenhas Dalle Nery <lmascare@fgrenoble.grenoble.grid5000.fr>
2025-05-22 13:48:17 +0200

    Transforming the experiment notebook into the original .py file

--- experiment_code/experiment.py
+++ experiment_code/experiment.py
@@ -1,13 +1,9 @@
-"""
-This code should be execute from the root of the project.
-All paths should be absolute paths.
-"""
-
 import execo
 import execo_g5k
 import execo_engine
 import time
 import os
+from dask.distributed import Client
 
 # this will be /home/lmascare
 HOME_DIR = os.path.expanduser("~")
@@ -19,213 +15,25 @@ PDI_DEISA_YML = HOME_DIR + "/bench/simulation/io_deisa.yml"
 SIM_EXECUTABLE = HOME_DIR + "/bench/simulation/build/main"
 DEISA_PATH = HOME_DIR + "/bench/deisa/"
 
+ANALYTICS_PY_FILE = HOME_DIR + "/bench/in-situ/bench_deisa.py"
+
+# must be in the same directory as the script/notebook
 SCHEDULER_FILE = HOME_DIR + "/bench/experiment/scheduler.json"
 
-OUTPUT_DIR = HOME_DIR + "/bench/experiment/output/"
+OUTPUT_DIR = HOME_DIR + "/bench/experiment/"
+
+# the total number of nodes needs to be THE SAME as the total number of workers passed in the analytics script
+DASK_WORKERS_PER_NODE = 2
+TOTAL_DASK_WORKERS = 2
 
 
-def run_experiment(nb_reserved_nodes):
-
-    # Getting the nodes
-    
-    jobs = execo_g5k.oarsub(
-        [
-            (
-                execo_g5k.OarSubmission(f"nodes={nb_reserved_nodes}", walltime=5*60),
-                "grenoble",
-            )
-        ]
-    )
-    
-    job_id, site = jobs[0]
-    print(f"Job {job_id} reserved on site {site}")
-    
-    nodes = execo_g5k.oar.get_oar_job_nodes(job_id, site)
-    head_node, nodes = nodes[0], nodes[1:]
-
-    print(f"Head node: {head_node}")
-    print(f"Other nodes: {nodes}")
-    
-
-    cores_per_node = execo_g5k.get_host_attributes(head_node)["architecture"]["nb_cores"]
-    total_simulation_cores = cores_per_node * len(nodes)
-
-    print(f"Total simulation cores: {total_simulation_cores}")
-
-
-    ##########################################################
-    #                   RUNNING SCHEDULER
-    ##########################################################
-    # Sending the commands for head node
-    
-    if os.path.exists(SCHEDULER_FILE):
-        os.remove(SCHEDULER_FILE)
-
-    #redirecting the output to a file
-    head_node_cmd = (
-        f"dask scheduler --scheduler-file {SCHEDULER_FILE}"
-    )
-    head_node_process = execo.SshProcess(
-        f'singularity exec {PATH_TO_SIF_FILE} bash -c "{head_node_cmd}"',
-        head_node,
-    )
-    head_node_process.start()
-
-    print()
-    print("#"*20)
-    print("Output:")
-    print(head_node_process.stdout)
-    print("-"*20)
-    print("Error:")
-    print(head_node_process.stderr)
-    print()
-    print("#"*20)
-
-    ##########################################################
-    #                   RUNNING SIMULATION
-    ##########################################################
-    
-    # Starting the simulation 32 - 30 for sim and 2 for dask 
-
-
-# """
-# export OMP_NUM_THREADS=2; \
-# export OMP_PROC_BIND=spread; \
-# export OMP_PLACES=threads; \
-# pdirun /home/lmascare/bench/simulation/build/main /home/lmascare/bench/simulation/setup.ini /home/lmascare/bench/simulation/io_deisa.yml > simulation_output.log 2>&1"
-
-# """
-    host_list = ",".join([f"{node.address}:{cores_per_node}" for node in nodes])
-    simulation_cmd = (
-        'export OMP_NUM_THREADS=2; '
-        'export OMP_PROC_BIND=spread; '
-        'export OMP_PLACES=threads; '
-        f'pdirun {SIM_EXECUTABLE} {SIMULATION_INI} {PDI_DEISA_YML} > simulation_output.log 2>&1'
-    )
-
-    # Build the command with two singularity exec calls:
-    mpi_cmd = (
-        f'singularity exec {PATH_TO_SIF_FILE} bash -c "'
-        f"mpirun -np 5 "#{total_simulation_cores} "
-        f"--host {host_list} "
-        f'singularity exec {PATH_TO_SIF_FILE} bash -c \'{simulation_cmd}\'"'
-    )
-
-    print("Simulation command:")
-    print(mpi_cmd)
-    mpi_process = execo.SshProcess(
-        mpi_cmd,
-        nodes[0],
-    )
-    mpi_process.start()
-
-    mpi_process.wait()
-
-    print("Output:")
-    print(mpi_process.stdout)
-    print("-"*20)
-    print("Error:")
-    print(mpi_process.stderr)
-    print()
-    print("#"*20)
-
-
-    
-    
-    #Working a bit - small bug in deisa, i guess
-    """
-    singularity exec docker/images/bench.sif bash -l -c "mpirun -np 4 --host dahu-12.grenoble.grid5000.fr:32,dahu-21.grenoble.grid5000.fr:32 bash -l -c 'pdirun ~/bench/simulation/build/main ~/bench/simulation/setup.ini ~/bench/simulation/io_deisa.yml' "
-    """
-
-    # I guess we dont need to execute singularity 2 times
-    """"
-    singularity exec docker/images/bench.sif bash -l -c "mpirun -np 4 --host dahu-12.grenoble.grid5000.fr:32,dahu-21.grenoble.grid5000.fr:32 bash -l -c 'which pdirun' "
-    """
-
-
-
-
-
-
-    # simulation_node_cmd = f"""mpirun --host {node_i.address} bash -c " """
-
-    # print(simulation_node_cmd)
-
-
-
-
-    
-    # configs = get_configs("simulation/setup.ini")
-    # NMPI = int(configs["mx"]) * int(configs["my"]) * int(configs["mz"])
-    # pre_cmd = f"export PYTHONPATH={DEISA_PATH}"
-    
-    
-    
-    # # Optionally, load additional simulation configuration from an ini file.
-    # configs = get_configs(SIM_SETUP_INI)
-    # NMPI = int(configs["mx"]) * int(configs["my"]) * int(configs["mz"])
-    # print(f"Simulation configuration NMPI (grid size product): {NMPI}")
-    
-    # # Build the complete MPI command.
-    # # You can adjust flags (e.g., --report-bindings) as necessary.
-    # mpi_cmd = (
-    #     f"mpirun --report-bindings -np {total_simulation_cores} "
-    #     f"--host {host_list} "
-    #     f"singularity exec {PATH_TO_SIF_FILE} {SIM_EXECUTABLE}"
-    # )
-
-
-
-
-
-
-
-
-    
-    # host = node_i.address
-    # cmd_to_run_simulation = (
-    #     f" mpirun --host {} -np {NMPI} pdirun "
-    #     f"{SIM_EXECUTABLE} {SIMULATION_INI} {PDI_DEISA_YML} --kokkos-map-device-id-by=mpi_rank "
-    # )
-
-    # new_process = execo.SshProcess(
-    #     f'singularity exec {PATH_TO_SIF_FILE} bash -c "{pre_cmd} && {cmd_to_run_simulation}"',
-    #     node_i
-    # )
-    # new_process.start()
-    # node_processes.append(new_process)
-
-    # time.sleep(1)
-
-    # for process_it in node_processes:
-    #     print("Process Output:")
-    #     print(process_it.stdout)
-
-    #     print()
-    #     print("Process Error:")
-    #     print(process_it.stderr)
-    
-    #time.sleep(1)
-
-    #print("Jobs from my user:")
-    #print(execo_g5k.get_current_oar_jobs())
-
-    #print("Waiting for the execution to finish...")
-    #head_node_process.wait()
-    
-    #print("#"*50);print("Command output:")
-    #print("#"*50);print(head_node_process.stdout);print("#"*50)
-    #print("Command error output:")
-    #print("#"*50);print(head_node_process.stderr);print("#"*50)
-
-
-""" 
-Get configs from a .ini file.
-
-Arguments:
-    config_file (str): Path to the .ini configuration file.
-"""
 def get_configs(config_file):
+    """ 
+    Get configs from a .ini file.
+
+    Arguments:
+        config_file (str): Path to the .ini configuration file.
+    """
     import configparser
 
     config = configparser.ConfigParser()
@@ -240,8 +48,181 @@ def get_configs(config_file):
 
     return configs
 
+#############################################
+#             Alloc the nodes
+#############################################
+configs = get_configs(SIMULATION_INI)
 
-if __name__ == "__main__":
-    configs = get_configs("simulation/setup.ini")
-    run_experiment(3)
-    
+nb_reserved_nodes = 1
+
+jobs = execo_g5k.oarsub(
+    [
+        (
+            execo_g5k.OarSubmission(f"nodes={nb_reserved_nodes}", walltime=60*60),
+            "grenoble",
+        )
+    ]
+)
+
+job_id, site = jobs[0]
+print(f"Job {job_id} reserved on site {site}")
+
+nodes = execo_g5k.oar.get_oar_job_nodes(job_id, site)
+head_node, nodes = nodes[0], nodes[1:]
+
+print(f"Head node: {head_node}")
+print(f"Other nodes: {nodes}")
+
+
+cores_per_node = execo_g5k.get_host_attributes(head_node)["architecture"]["nb_cores"]
+total_simulation_cores = cores_per_node * len(nodes)
+
+print(f"Total simulation cores: {total_simulation_cores}")
+
+
+import socket
+
+head_node_ip = socket.gethostbyname(head_node.address)
+
+nodes_ips = []
+for node in nodes:
+    nodes_ips.append(socket.gethostbyname(node.address))
+print(f"Head node IP: {head_node_ip}")
+print(f"Other nodes IPs: {nodes_ips}")
+
+
+
+##########################################################
+#                   RUNNING SCHEDULER
+##########################################################
+# Sending the commands for head node
+
+print("Initializing the scheduler...")
+
+if os.path.exists(SCHEDULER_FILE):
+    os.remove(SCHEDULER_FILE)
+
+#redirecting the output to a file
+scheduler_cmd = (
+    f"dask scheduler "
+    # f"--host {head_node.address} "
+    f"--scheduler-file {SCHEDULER_FILE} "
+    f"> {OUTPUT_DIR}scheduler.e 2>&1; "
+    "sync"
+)
+
+scheduler_process = execo.SshProcess(
+    f'singularity exec {PATH_TO_SIF_FILE} bash -c "{scheduler_cmd}"',
+    head_node,
+)
+scheduler_process.start()
+
+# Wait for the scheduler to start by checking the scheduler file
+while not os.path.exists(SCHEDULER_FILE):
+    time.sleep(1)
+
+print("Scheduler started!")
+
+##########################################################
+#                   RUNNING WORKERS
+##########################################################
+
+print("Initializing the workers...")
+
+worker_cmd = (
+    f"dask worker "
+    # f"tcp://{head_node_ip}:8786 "
+    # f"--dashboard-address {head_node.address}:8787 "
+    f"--nworkers {DASK_WORKERS_PER_NODE} "
+    "--nthreads 1 "
+    "--local-directory /tmp "
+    f"--scheduler-file {SCHEDULER_FILE} "
+    f"> {OUTPUT_DIR}worker.e 2>&1"
+)
+
+# for node in nodes:
+    # redirecting the output to a file
+worker_process = execo.SshProcess(
+    f'singularity exec {PATH_TO_SIF_FILE} bash -c "{worker_cmd}"',
+    head_node,
+)
+worker_process.start()
+
+print("Workers started!")
+
+
+##########################################################
+#                   RUNNING ANALYTICS
+##########################################################
+
+# python3 $OPWD/in-situ/bench_deisa.py $NDASKWORKERS $SCHEFILE 2>analytics.e&
+
+print("Initializing the analytics...")
+
+py_cmd = (
+    'export PYTHONPATH=/home/lmascare/bench/deisa/:$PYTHONPATH; '
+    f'python3 {ANALYTICS_PY_FILE} {TOTAL_DASK_WORKERS} {SCHEDULER_FILE} > {OUTPUT_DIR}analytics.e 2>&1'
+)
+analytics_cmd = (
+    # 'hostname'
+    f'singularity exec {PATH_TO_SIF_FILE} bash -c "{py_cmd}"'
+
+)
+print("Analytics command:")
+print(analytics_cmd)
+
+analytics_process = execo.SshProcess(
+    analytics_cmd,
+    head_node,
+)
+analytics_process.start()
+# analytics_process.wait()
+
+print("Analytics started!")
+
+
+##########################################################
+#                   RUNNING SIMULATION
+##########################################################
+
+print("Initializing the simulation...")
+
+# Starting the simulation 32 - 30 for sim and 2 for dask 
+
+mx = int(configs["mx"])
+my = int(configs["my"])
+mz = int(configs["mz"])
+
+MPI_NP = mx * my * mz
+
+print(f"Running simulation with {MPI_NP} MPI processes")
+
+host_list = ",".join([f"{node.address}:{2}" for node in nodes])
+simulation_cmd = (
+    f'export PYTHONPATH=/home/lmascare/bench/deisa/:$PYTHONPATH; '
+    f'pdirun {SIM_EXECUTABLE} {SIMULATION_INI} {PDI_DEISA_YML} --kokkos-map-device-id-by=mpi_rank > {OUTPUT_DIR}simulation.e 2>&1'
+)
+
+# Build the command with two singularity exec calls:
+mpi_cmd = (
+    'export OMP_NUM_THREADS=2; '
+    'export OMP_PROC_BIND=spread; '
+    'export OMP_PLACES=threads; '
+    f"mpirun -np {MPI_NP} "
+    # f"--host {host_list} "
+    f'singularity exec {PATH_TO_SIF_FILE} bash -c "{simulation_cmd}"'
+)
+
+print("Simulation command:")
+print(mpi_cmd)
+mpi_process = execo.SshProcess(
+    mpi_cmd,
+    head_node,
+)
+mpi_process.start()
+
+print("Analytics started!")
+
+print("Waiting for the simulation to finish...")
+mpi_process.wait()
+print("Simulation finished!")
