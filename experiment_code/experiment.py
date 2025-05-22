24867d5111ea8db8837e79fb3851a33314b099b8
Luiz Felipe Mascarenhas Dalle Nery <lmascare@fgrenoble.grenoble.grid5000.fr>
2025-05-22 15:04:43 +0200

    Changing the policy for allocating the mpi jobs to round robin

--- experiment_code/experiment.py
+++ experiment_code/experiment.py
@@ -54,7 +54,7 @@ def get_configs(config_file):
 
 configs = get_configs(SIMULATION_INI)
 
-nb_reserved_nodes = 3
+nb_reserved_nodes = 2 + 1 # execution nodes + 1 head node
 
 jobs = execo_g5k.oarsub(
     [
@@ -91,148 +91,154 @@ for node in nodes:
 print(f"Head node IP: {head_node_ip}")
 print(f"Other nodes IPs: {nodes_ips}")
 
+try:
 
+    ##########################################################
+    #                   RUNNING SCHEDULER
+    ##########################################################
+    # Sending the commands for head node
 
-##########################################################
-#                   RUNNING SCHEDULER
-##########################################################
-# Sending the commands for head node
+    print("Initializing the scheduler...")
 
-print("Initializing the scheduler...")
+    if os.path.exists(SCHEDULER_FILE):
+        os.remove(SCHEDULER_FILE)
 
-if os.path.exists(SCHEDULER_FILE):
-    os.remove(SCHEDULER_FILE)
-
-#redirecting the output to a file
-scheduler_cmd = (
-    f"dask scheduler "
-    # f"--host {head_node.address} "
-    f"--scheduler-file {SCHEDULER_FILE} "
-    f"> {OUTPUT_DIR}scheduler.e 2>&1; "
-    "sync"
-)
-
-scheduler_process = execo.SshProcess(
-    f'singularity exec {PATH_TO_SIF_FILE} bash -c "{scheduler_cmd}"',
-    head_node,
-)
-scheduler_process.start()
-
-# Wait for the scheduler to start by checking the scheduler file
-while not os.path.exists(SCHEDULER_FILE):
-    time.sleep(1)
-
-print("Scheduler started!")
-
-##########################################################
-#                   RUNNING WORKERS
-##########################################################
-
-print("Initializing the workers...")
-
-worker_cmd = (
-    f"dask worker "
-    f"tcp://{head_node_ip}:8786 "
-    # f"--dashboard-address {head_node.address}:8787 "
-    f"--nworkers {DASK_WORKERS_PER_NODE} "
-    "--nthreads 1 "
-    "--local-directory /tmp "
-    f"--scheduler-file {SCHEDULER_FILE} "
-    f"> {OUTPUT_DIR}worker.e 2>&1"
-)
-
-# for node in nodes:
-    # redirecting the output to a file
-for node in nodes:
-    worker_process = execo.SshProcess(
-        f'singularity exec {PATH_TO_SIF_FILE} bash -c "{worker_cmd}"',
-        node,
+    #redirecting the output to a file
+    scheduler_cmd = (
+        f"dask scheduler "
+        # f"--host {head_node.address} "
+        f"--scheduler-file {SCHEDULER_FILE} "
+        f"> {OUTPUT_DIR}scheduler.e 2>&1; "
+        "sync"
     )
-    worker_process.start()
 
-print("Workers started!")
+    scheduler_process = execo.SshProcess(
+        f'singularity exec {PATH_TO_SIF_FILE} bash -c "{scheduler_cmd}"',
+        head_node,
+    )
+    scheduler_process.start()
+
+    # Wait for the scheduler to start by checking the scheduler file
+    while not os.path.exists(SCHEDULER_FILE):
+        time.sleep(1)
+
+    print("Scheduler started!")
+
+    ##########################################################
+    #                   RUNNING WORKERS
+    ##########################################################
+
+    print("Initializing the workers...")
+
+    worker_cmd = (
+        f"dask worker "
+        f"tcp://{head_node_ip}:8786 "
+        # f"--dashboard-address {head_node.address}:8787 "
+        f"--nworkers {DASK_WORKERS_PER_NODE} "
+        "--nthreads 1 "
+        "--local-directory /tmp "
+        f"--scheduler-file {SCHEDULER_FILE} "
+        f"> {OUTPUT_DIR}worker.e 2>&1"
+    )
+
+    # for node in nodes:
+        # redirecting the output to a file
+    for node in nodes:
+        worker_process = execo.SshProcess(
+            f'singularity exec {PATH_TO_SIF_FILE} bash -c "{worker_cmd}"',
+            node,
+        )
+        worker_process.start()
+
+    print("Workers started!")
 
 
-##########################################################
-#                   RUNNING ANALYTICS
-##########################################################
+    ##########################################################
+    #                   RUNNING ANALYTICS
+    ##########################################################
 
-# python3 $OPWD/in-situ/bench_deisa.py $NDASKWORKERS $SCHEFILE 2>analytics.e&
+    # python3 $OPWD/in-situ/bench_deisa.py $NDASKWORKERS $SCHEFILE 2>analytics.e&
 
-print("Initializing the analytics...")
+    print("Initializing the analytics...")
 
-py_cmd = (
-    'export PYTHONPATH=/home/lmascare/bench/deisa/:$PYTHONPATH; '
-    f'python3 {ANALYTICS_PY_FILE} {TOTAL_DASK_WORKERS} {SCHEDULER_FILE} > {OUTPUT_DIR}analytics.e 2>&1'
-)
-analytics_cmd = (
-    # 'hostname'
-    f'singularity exec {PATH_TO_SIF_FILE} bash -c "{py_cmd}"'
+    py_cmd = (
+        'export PYTHONPATH=/home/lmascare/bench/deisa/:$PYTHONPATH; '
+        f'python3 {ANALYTICS_PY_FILE} {TOTAL_DASK_WORKERS} {SCHEDULER_FILE} > {OUTPUT_DIR}analytics.e 2>&1'
+    )
+    analytics_cmd = (
+        # 'hostname'
+        f'singularity exec {PATH_TO_SIF_FILE} bash -c "{py_cmd}"'
 
-)
-print("Analytics command:")
-print(analytics_cmd)
+    )
+    print("Analytics command:")
+    print(analytics_cmd)
 
-analytics_process = execo.SshProcess(
-    analytics_cmd,
-    head_node,
-)
-analytics_process.start()
-# analytics_process.wait()
+    analytics_process = execo.SshProcess(
+        analytics_cmd,
+        head_node,
+    )
+    analytics_process.start()
+    # analytics_process.wait()
 
-print("Analytics started!")
+    print("Analytics started!")
 
 
-##########################################################
-#                   RUNNING SIMULATION
-##########################################################
+    ##########################################################
+    #                   RUNNING SIMULATION
+    ##########################################################
 
-print("Initializing the simulation...")
+    print("Initializing the simulation...")
 
-# Starting the simulation 32 - 30 for sim and 2 for dask 
+    # Starting the simulation 32 - 30 for sim and 2 for dask 
 
-mx = int(configs["mx"])
-my = int(configs["my"])
-mz = int(configs["mz"])
+    mx = int(configs["mx"])
+    my = int(configs["my"])
+    mz = int(configs["mz"])
 
-MPI_NP = mx * my * mz
+    MPI_NP = mx * my * mz
 
-print(f"Running simulation with {MPI_NP} MPI processes")
+    print(f"Running simulation with {MPI_NP} MPI processes")
 
-host_list = ",".join([f"{node.address}:{cores_per_node}" for node in nodes])
-simulation_cmd = (
-    f'export PYTHONPATH=/home/lmascare/bench/deisa/:$PYTHONPATH; '
-    f'pdirun {SIM_EXECUTABLE} {SIMULATION_INI} {PDI_DEISA_YML} --kokkos-map-device-id-by=mpi_rank > {OUTPUT_DIR}simulation.e 2>&1'
-)
+    host_list = ",".join([f"{node.address}:{cores_per_node}" for node in nodes])
+    # host_list = ",".join([f"{node.address}" for node in nodes])
+    simulation_cmd = (
+        f'export PYTHONPATH=/home/lmascare/bench/deisa/:$PYTHONPATH; '
+        f'pdirun {SIM_EXECUTABLE} {SIMULATION_INI} {PDI_DEISA_YML} --kokkos-map-device-id-by=mpi_rank > {OUTPUT_DIR}simulation.e 2>&1'
+    )
 
-# Build the command with two singularity exec calls:
-mpi_cmd = (
-    'export OMP_NUM_THREADS=2; '
-    'export OMP_PROC_BIND=spread; '
-    'export OMP_PLACES=threads; '
-    f"mpirun -np {MPI_NP} "
-    # f"--host {host_list} "
-    f'singularity exec {PATH_TO_SIF_FILE} bash -c "{simulation_cmd}"'
-)
+    # Build the command with two singularity exec calls:
+    mpi_cmd = (
+        'export OMP_NUM_THREADS=2; '
+        'export OMP_PROC_BIND=spread; '
+        'export OMP_PLACES=threads; '
+        f"mpirun -np {MPI_NP} "
+        f"--map-by slot "
+        f"--host {host_list} "
+        f'singularity exec {PATH_TO_SIF_FILE} bash -c "{simulation_cmd}"'
+    )
 
-print("Simulation command:")
-print(mpi_cmd)
-mpi_process = execo.SshProcess(
-    mpi_cmd,
-    nodes[0],
-)
-mpi_process.start()
+    print("Simulation command:")
+    print(mpi_cmd)
+    mpi_process = execo.SshProcess(
+        mpi_cmd,
+        nodes[0],
+    )
+    mpi_process.start()
 
-print("Analytics started!")
+    print("Analytics started!")
 
-print("Waiting for the simulation to finish...")
-mpi_process.wait()
-print("Simulation finished!")
-# Wait for the analytics to finish
-print("Waiting for the analytics to finish...")
-analytics_process.wait()
-print("Analytics finished!")
+    print("Waiting for the simulation to finish...")
+    mpi_process.wait()
+    print("Simulation finished!")
+    # Wait for the analytics to finish
+    print("Waiting for the analytics to finish...")
+    analytics_process.wait()
+    print("Analytics finished!")
 
-execo_g5k.oardel(jobs)
+except Exception as e:
+    print(f"An error occurred: {e}")
 
-print("Job deleted!")
+finally:
+    # Clean up the job
+    execo_g5k.oardel(jobs)
+    print("Job deleted!")
